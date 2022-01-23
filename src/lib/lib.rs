71470d67132cdd8c2fac2bc4902a7f71607b651c
durid-ah <ahmad.durid96@gmail.com>
2022-01-23 09:32:58 -0700

    added find_location to the node methods as well as its tests

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,3 +1,5 @@
+use std::cell::RefCell;
+use std::rc::Rc;
 use node::{Node, NodeRef};
 
 mod node;
@@ -6,3 +8,25 @@ pub struct BTree {
     order: usize,
     root: NodeRef
 }
+
+impl BTree {
+    pub fn new(order: usize) -> Self {
+        return Self {
+            order,
+            root: Rc::new(RefCell::new(Node::new(order)))
+        }
+    }
+
+    pub fn add(value: usize) {
+
+    }
+}
+
+/// Insert value
+/// 1. Check if it fits in the first node (binary search style)
+/// 2. Check if it fits on the child between the two keys (lager than the largest key?)
+/// 3. If it
+fn hello() {
+    return;
+}
+
