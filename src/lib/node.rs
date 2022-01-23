d2bc305e9ddeaac3179b5fde70b0a3cce52e4fd5
durid-ah <ahmad.durid96@gmail.com>
2022-01-23 09:39:03 -0700

    renamed find_location to find_future_key_location and added documentation

--- src/lib/node.rs
+++ src/lib/node.rs
@@ -16,7 +16,7 @@ impl Node {
    pub fn new(order: usize) -> Self {
       let min_child_count: usize = order / 2;
 
-      return Self{
+      return Self {
          parent: Option::None,
          keys: Vec::with_capacity(order - 1),
          children: Option::None,
@@ -24,6 +24,7 @@ impl Node {
          min_child_count
       }
    }
+
    // pub fn add_key(&mut self, key: usize) {
    //    // if the value already exists just update the location
    //    if let Option::Some(idx) = self.find_key(key) {
@@ -76,12 +77,53 @@ impl Node {
          } else {
             start = mid + 1;
          }
-
       }
 
       return Option::None;
    }
 
+   /// Find the index where the new key would reside or an error with the
+   /// index where it already exists
+   pub fn find_future_key_index(&self, key: usize) -> Result<usize, (usize, String)> {
+      let calculate_mid =
+         |start: isize, end: isize| -> isize { ((end - start) / 2) + start };
+
+      if self.keys.len() == 0 {
+         return Ok(0);
+      }
+
+      let mut start = 0 as isize;
+      let key_length = self.keys.len();
+      let mut end = (key_length - 1) as isize;
+
+      while end >= start {
+         let mid: isize = calculate_mid(start, end);
+         let mid_idx = mid as usize;
+
+         if self.keys[mid_idx] == key {
+            return Err((mid_idx, format!("value already exists at {}", mid_idx)))
+         }
+         else if mid_idx == 0 && self.keys[mid_idx] > key {
+            return Ok(mid_idx)
+         }
+         else if mid_idx == (key_length - 1) && self.keys[mid_idx] < key {
+            return Ok(mid_idx + 1)
+         }
+         else if self.keys[mid_idx] > key && self.keys[mid_idx - 1] < key {
+            return Ok(mid_idx);
+         }
+
+         if self.keys[mid_idx] > key {
+            end = mid - 1;
+         } else {
+            start = mid + 1;
+         }
+      }
+
+      panic!("Unable to find value {}", key)
+   }
+
+   fn s(){
    // pub fn split_node(&mut self) -> (usize, Node, Node) {
    //    let key_len = self.keys.len();
    //    let child_len = self.children.len();
@@ -104,6 +146,7 @@ impl Node {
    //
    //    (mid_key, self)
    // }
+   }
 
    pub fn get_key(&self, index: usize) -> usize { self.keys[index] }
 
@@ -245,9 +288,85 @@ mod tests {
    }
 
    #[test]
-   fn looper() {
-      for i in (1..5).rev() {
-         println!("{:?}", i);
+   fn find_location_in_even_vector() {
+      let mut node = Node::new(5);
+      node.keys = vec![5, 10, 15, 20];
+
+      match node.find_future_key_index(3) {
+         Ok(index) => assert_eq!(index, 0, "Value must be 0 instead got {}", index),
+         Err(_) => assert!(false, "Value")
+      }
+
+      match node.find_future_key_index(8) {
+         Ok(index) => assert_eq!(index, 1, "Value must be 1 instead got {}", index),
+         Err(_) => assert!(false, "Value")
+      }
+
+      match node.find_future_key_index(11) {
+         Ok(index) => assert_eq!(index, 2, "Value must be 2 instead got {}", index),
+         Err(_) => assert!(false, "Value")
+      }
+
+      match node.find_future_key_index(18) {
+         Ok(index) => assert_eq!(index, 3, "Value must be 3 instead got {}", index),
+         Err(_) => assert!(false, "Value")
+      }
+
+      match node.find_future_key_index(25) {
+         Ok(index) => assert_eq!(index, 4, "Value must be 4 instead got {}", index),
+         Err(_) => assert!(false, "Value")
+      }
+   }
+
+   #[test]
+   fn find_location_in_odd_vector() {
+      let mut node = Node::new(5);
+      node.keys = vec![5, 10, 15, 20, 25];
+
+      match node.find_future_key_index(3) {
+         Ok(index) => assert_eq!(index, 0, "Value must be 0 instead got {}", index),
+         Err(_) => assert!(false, "Value")
+      }
+
+      match node.find_future_key_index(8) {
+         Ok(index) => assert_eq!(index, 1, "Value must be 1 instead got {}", index),
+         Err(_) => assert!(false, "Value")
+      }
+
+      match node.find_future_key_index(11) {
+         Ok(index) => assert_eq!(index, 2, "Value must be 2 instead got {}", index),
+         Err(_) => assert!(false, "Value")
+      }
+
+      match node.find_future_key_index(18) {
+         Ok(index) => assert_eq!(index, 3, "Value must be 3 instead got {}", index),
+         Err(_) => assert!(false, "Value")
+      }
+
+      match node.find_future_key_index(23) {
+         Ok(index) => assert_eq!(index, 4, "Value must be 4 instead got {}", index),
+         Err(_) => assert!(false, "Value")
+      }
+
+      match node.find_future_key_index(26) {
+         Ok(index) => assert_eq!(index, 5, "Value must be 5 instead got {}", index),
+         Err(_) => assert!(false, "Value")
+      }
+   }
+
+   #[test]
+   fn find_location_in_single_element() {
+      let mut node = Node::new(5);
+      node.keys = vec![5];
+
+      match node.find_future_key_index(3) {
+         Ok(index) => assert_eq!(index, 0, "Value must be 0 instead got {}", index),
+         Err(_) => assert!(false, "Value")
+      }
+
+      match node.find_future_key_index(8) {
+         Ok(index) => assert_eq!(index, 1, "Value must be 1 instead got {}", index),
+         Err(_) => assert!(false, "Value")
       }
    }
 }
