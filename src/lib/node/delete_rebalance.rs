8a9f370b0553b75d540fd755b4dddac37e09b73c
durid-ah <ahmad.durid96@gmail.com>
2022-05-01 20:35:48 -0600

    created file to handle rebalance logic

---
+++ src/lib/node/delete_rebalance.rs
@@ -0,0 +1,9 @@
+
+
+fn rebalance_after_delete()
+{
+   // TODO: Split if full after merging the children
+   //    - If it has children borrow
+   //    - Figure out how to merge the leaf logic with the other
+   //    stuff
+}
